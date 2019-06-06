719bf64fcb908ff68f9414c1a2f8cdca005818ef
grunnpy <33801576+grunnpy@users.noreply.github.com>
2019-06-06 09:11:20 +0200

    day 3
    weird slideshow order

--- script.js
+++ script.js
@@ -18,11 +18,12 @@ function topFunction() {
 
 // SLIDESHOWS IN LAB PAGE
 
-var slideIndex = [1,1];
+var slideIndex = [1,2];
 /* Class the members of each slideshow group with different CSS classes */
-var slideId = ["mySlides1", "mySlides2"] 
+var slideId = ["mySlides1", "mySlides2", "mySlides3"] 
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
@@ -31,8 +32,8 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
   var i;
   var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1} 
-  if (n < 1) {slideIndex[no] = x.length}
+  if (n > x.length) {slideIndex[no] = 2} 
+  if (n < 2) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none"; 
   }
