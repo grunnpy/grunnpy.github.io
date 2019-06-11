388cd93fbeb1a8fef4b1739ffa76dca3e6f578cc
grunnpy <33801576+grunnpy@users.noreply.github.com>
2019-06-11 21:39:22 +0200

    Day 6

--- script.js
+++ script.js
@@ -18,14 +18,15 @@ function topFunction() {
 
 // SLIDESHOWS IN LAB PAGE
 
-var slideIndex = [1, 1, 1, 1, 1];
+var slideIndex = [1, 1, 1, 1, 1, 1];
 /* Class the members of each slideshow group with different CSS classes */
-var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5"] 
+var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6"] 
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
 showSlides(1, 3);
 showSlides(1, 4);
+showSlides(1, 5);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
